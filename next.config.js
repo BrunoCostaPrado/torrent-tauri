3b6b948bf4022ec190e9592f1f96168efc411309
Bruno Costa Prado <brunocostaprado@gmail.com>
2025-05-22 22:30:32 -0300

    fist commit

--- next.config.js
+++ next.config.js
@@ -2,9 +2,25 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import "./src/env.js"
+const isProd = process.env.NODE_ENV === "production"
+
+const internalHost = process.env.TAURI_DEV_HOST || "localhost"
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	experimental: {
+		nextScriptWorkers: true,
+	},
+	// trailingSlash: true,
+	// compress: true,
+	// cacheMaxMemorySize: 100,
 
-export default config;
+	// output: "export",
+	images: {
+		unoptimized: true,
+	},
+	assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
+}
+
+export default config
